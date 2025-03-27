30ec7972cb69d4bab691ba4cda80bfeda09d4c11
daeoulee <daoule@naver.com>
2025-03-27 14:04:12 +0900

    3월 27일 수업

---
+++ build_practce/src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    class InnerHello{
+            public static void main(String[] args) {
+                System.out.println("I N N E R  |  H E L L O ");
+            }
+        }
+        
+    }
+    
+
